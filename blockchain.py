b57e2f9e27b432ee90bf3809999c23200d586648
joshcoen <josh.coen@gmail.com>
2017-11-27 15:23:04 -0800

    initial blockchain python file

---
+++ blockchain.py
@@ -0,0 +1,16 @@
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
